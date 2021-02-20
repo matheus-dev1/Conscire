adc459331d528b0a68184a6480cc45f65a23420c
Sabrina-AP <72463853+Sabrina-AP@users.noreply.github.com>
2021-02-20 03:20:24 -0300

    Atualização: conexão com BD

--- frontend/src/pages/contato/index.js
+++ frontend/src/pages/contato/index.js
@@ -1,11 +1,54 @@
 import React from 'react'
-
+import {useState, useEffect} from 'react';  
+import Axios from "axios";
 import Header from '../../components/Header'
 import Footer from '../../components/Footer'
 
-// import './styles.css'
-
 function Contato() {
+    const [nome, setNome] = useState("");
+    const [sobrenome, setSobrenome] = useState("");
+    const [msg, setMsg] = useState("");
+    const [erros, setErros] = useState([]);
+    const [statusErro, setStatusErro] = useState(false);
+    const [mensagens, setMensagens ] = useState([]);
+    const v = []
+
+    useEffect(() => {
+      async function fetchData(){
+          const url =  "http://localhost:5000/comentarios/retorna"; 
+          const response = await fetch(url);
+          setMensagens(await response.json());
+      }fetchData();    
+    }, []);
+
+    const mensagem = ()=>{
+        Axios.post("http://localhost:5000/comentarios/envia", {
+            nome: nome,
+            sobrenome: sobrenome,
+            msg: msg
+        }).then((response) => {
+          if(response.data.message){
+            v.push(response.data.message)
+            setStatusErro(true);
+        }else{
+          for(var i=0;i<response.data.validacao.errors.length;i++){
+            if(!response.data.auth){
+                v.push(response.data.validacao.errors[i].msg)
+                setStatusErro(true);
+            }
+          }
+       }
+        setErros(v)
+    })
+        setTimeout(() => {
+            setStatusErro(false);
+          }, 10000);
+
+      setNome('');
+      setSobrenome('');
+      setMsg('');
+    }
+
     return(
       <div>
         <Header />
@@ -22,24 +65,30 @@ function Contato() {
                       </div>
                     </div>
 
-                    <form action="#" method="POST">
+                    {erros.map((item) =>{
+                      return(
+                        statusErro &&  <div class="alert alert-danger mx-auto mt-4 w-75" role="alert">{item}</div>
+                      )            
+                    })}
+
+                    <div>
                       <div class="form p-3">
                         <div class="form-row my-5">
 
                           <div class="col-lg-6">
-                            <input type="text" class="effect-1" placeholder="Nome" name='nome'/>
+                            <input type="text" class="effect-1" value={nome} onChange={event => setNome(event.target.value)} pattern="^[^-\s][a-zA-ZÀ-ú ]*" placeholder="Nome" name='nome' required/>
                             <span class="Focus-border"></span>
                           </div>
 
                           <div class="col-lg-6">
-                            <input type="text" class="effect-1" placeholder="Sobrenome"/>
+                            <input type="text" class="effect-1"  name='sobrenome' value={sobrenome} onChange={event => setSobrenome(event.target.value)} pattern="^[^-\s][a-zA-ZÀ-ú ]*" placeholder="sobrenome" required/>
                             <span class="Focus-border"></span>
                           </div>
                         </div>
 
                         <div class="form-row pt-5">
                           <div class="col-lg-12">
-                            <textarea name="msg" cols="60" rows="4" placeholder= "Mensagem"></textarea>
+                            <textarea name="msg" value={msg} onChange={event => setMsg(event.target.value)} cols="60" rows="4" placeholder= "msg" required></textarea>
                             <span class="Focus-border"></span>
                           </div>
                       </div>
@@ -51,11 +100,13 @@ function Contato() {
                         </div>
 
                       <div class= "offset-2 col-lg-4">
-                        <button class="btn1">ENVIAR</button>
+                        <button onClick={mensagem} class="btn1">ENVIAR</button>
                       </div>
                   </div>
+                  
                 </div>
-                  </form>
+
+                  </div>
                           <p class="text-center text-dark">Entre em contato conosco: <br/>
                           Telefone: (11)3656-5562 <br/>
                           Endereço: Rua Conscire, 569 <br/>
@@ -64,10 +115,28 @@ function Contato() {
                   </div>
               </div>
             </div>
+           
+
           </section>
+          <div class="text-center text-dark py-3"> <h4 > Mensagens </h4> </div>
+            <div >          
+                {mensagens.map((item) =>{
+                    return(
+                      <div class="alert alert-success mx-auto mt-4 w-75" key={item.id}>
+                          <hr/><hr/>
+                          <div>
+                              Data: {item.data}
+                          </div>
+                          <div>
+                              Cliente {item.nome} {item.sobrenome} escreveu: {item.msg}
+                          </div>
+                          <hr/><hr/>
+                      </div>
+                    )            
+                })}
+            </div>
       <Footer />
     </div>
-    )
+  )
 }
-
 export default Contato
