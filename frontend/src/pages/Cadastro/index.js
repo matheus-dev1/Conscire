361b5dc8d1f988b67a32678cb0392e13c6fd050a
Sabrina-AP <72463853+Sabrina-AP@users.noreply.github.com>
2021-02-19 21:51:19 -0300

    Atualização: conexão com BD

--- frontend/src/pages/Cadastro/index.js
+++ frontend/src/pages/Cadastro/index.js
@@ -1,13 +1,54 @@
-/* eslint-disable jsx-a11y/alt-text */
-/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react'
+import {useState} from 'react';  
+ 
+import Axios from "axios";
 
 import Header from '../../components/Header'
 import Footer from '../../components/Footer'
 
-// import './styles.css'
 
 function Cadastro() {
+
+    const [nome, setNome] = useState("");
+    const [email, setEmail] = useState("");
+    const [senha, setSenha] = useState("");
+    const [confirme, setConfirme] = useState("");
+    const [erros, setErros] = useState([]);
+    const [statusErro, setStatusErro] = useState(false);
+    const v = []
+
+    const cadastrar = ()=>{
+        Axios.post("http://localhost:5000/register", {
+            nome: nome,
+            email: email,
+            senha: senha,
+            confirme: confirme
+        }).then((response) => {
+            
+            for (var i=0;i<response.data.validacao.errors.length;i++){
+                if(!response.data.auth){
+                    v.push(response.data.validacao.errors[i].msg)
+                    setStatusErro(true);
+                }else{
+                    setStatusErro(false);
+                }
+            }
+            
+            setErros(v)
+            console.log(erros)
+        })
+        setTimeout(() => {
+            setStatusErro(false);
+          }, 10000);
+        
+        setNome('');
+        setEmail('');
+        setSenha('');
+        setConfirme('');
+
+    }
+
+
     return (
         <>
             <Header />
@@ -21,34 +62,34 @@ function Cadastro() {
                     <img id="logo" src={require('../../assets/images/logo.png').default} alt="Conscire"/>
                     <h5 class= "my-3 p-2">Entre na sua conta</h5>      
                 
-                    <form action="#" method="POST">
+                    <div >
                         <div class="form-row">
                             <div class="col-lg-7">
-                            <input type="text" id="nome" name="nome" placeholder="Nome Completo" class="form-control my-3 p-4"/>
+                            <input type="text" id="nome" name="nome" value={nome} onChange={event => setNome(event.target.value)} pattern="^[^-\s][a-zA-ZÀ-ú ]*" required placeholder="Nome Completo" class="form-control my-3 p-4"/>
                             </div>
                         </div>
 
                         <div class="form-row">
                             <div class="col-lg-7">
-                        <input type="email" id="email" name="email" placeholder="Seu email aqui" class="form-control my-3 p-4"/>
+                        <input type="email" id="email" name="email" value={email} onChange={event => setEmail(event.target.value)} required placeholder="Seu email aqui" class="form-control my-3 p-4"/>
                         </div>
                     </div>
 
                     <div class="form-row">
                         <div class="col-lg-7">
-                        <input type="password" name="senha" id="senha" placeholder="Digite uma senha" class="form-control my-3 p-4"/>
+                        <input type="password" name="senha" id="senha" value={senha} onChange={event => setSenha(event.target.value)} required placeholder="Digite uma senha" class="form-control my-3 p-4"/>
                         </div>
                     </div>
 
                     <div class="form-row">
                         <div class="col-lg-7">
-                        <input type="password" name="confirme" placeholder="Confirme a senha" class="form-control my-3 p-4"/>
+                        <input type="password" name="confirme" id="confirme" value={confirme} onChange={event => setConfirme(event.target.value)} required placeholder="Digite a mesma senha" class="form-control my-3 p-4"/>
                         </div>
                     </div>
 
                     <div class="form-row">
                     <div class="col-lg-7">
-                    <button type="submit" class="btn1 mt-3 mb-5">Cadastrar</button>
+                    <button onClick={cadastrar} class="btn1 mt-3 mb-5">Cadastrar</button>
                     </div>
                     </div>
 
@@ -56,10 +97,15 @@ function Cadastro() {
                         Já possui uma conta? <br/> <a href="#"> Faca o login aqui</a>
                     </p>
 
-                    </form>
                     </div>
                     </div>
                     </div>
+                    </div>
+                    {erros.map((item) =>{
+                          return(
+                            statusErro &&  <div class="alert alert-danger mx-auto mt-4 w-75" role="alert">{item}</div>
+                          )            
+                      })}
                 </section>
             <Footer />
         </>
@@ -67,3 +113,8 @@ function Cadastro() {
 }
 
 export default Cadastro
+
+
+
+
+
