c5e8efe17f45c1f83e2a81efd08b949ef646b91e
Sabrina-AP <72463853+Sabrina-AP@users.noreply.github.com>
2021-02-25 14:27:58 -0300

    Referências

--- frontend/src/pages/ResultadoAudit/index.js
+++ frontend/src/pages/ResultadoAudit/index.js
@@ -36,11 +36,18 @@ function ResultadoAudit() {
                 <div >          
                     {
                         statusResult &&  <div className="alert alert-success mx-auto mt-4 w-75" role="alert">
-                        <p>{result}</p>
+                        <p className="text-center text-dark py-3">{result}</p>
                     </div>
                     }
                     </div>
                 </div>
+        
+                <p className="text-center text-dark py-3"> *As 'Definições' são apresentadas em:<br />
+                    CARNEIRO, A. P. L.; RONZANI, T. M.; AVALLONE, D. M.; FORMIGONI, M. L. O. S. Audit e Audit-C. Informações do teste AUDIT traduzidas para o português. Disponível em: <a href="https://edisciplinas.usp.br/pluginfile.php/4170599/mod_resource/content/1/audit.pdf">https://edisciplinas.usp.br/pluginfile.php/4170599/mod_resource/content/1/audit.pdf</a>. Acesso em: 2 out. 2020.
+                    <br />
+                    *As 'Intervenções' são apresentadas em:<br />
+                    BABOR, T.F.; HIGGINS-BIDDLE, J. C.; SAUNDERS, J. B.; MONTEIRO, M. G. The Alcohol Use Disorders Identification Test Guidelines for Use in Primary Care. 2001. <a href="https://apps.who.int/iris/bitstream/handle/10665/67205/WHO_MSD_MSB_01.6a.pdf?sequence=1">https://apps.who.int/iris/bitstream/handle/10665/67205/WHO_MSD_MSB_01.6a.pdf?sequence=1</a>. Tradução: REMELHE, J.; TOPA, J. Intervenções breves para o consumo de risco e nocivo de bebidas alcoólicas: guia para utilização em Cuidados Primários de Saúde. Informações sobre as 4 Zonas de Risco. Disponível em: <a href="https://apps.who.int/iris/bitstream/handle/10665/67210/WHO_MSD_MSB_01.6b_por.pdf?sequence=2&isAllowed=y">https://apps.who.int/iris/bitstream/handle/10665/67210/WHO_MSD_MSB_01.6b_por.pdf?sequence=2&isAllowed=y</a>. Acesso em: 2 out. 2020.  
+                </p>
                 
             <Footer />
         </>
